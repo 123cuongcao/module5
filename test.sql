d8351875fe76c0c85440fa1120abd9de589ffb05
DESKTOP-ACRT2MR\User <=>
2023-10-31 11:46:50 +0700

    first commit

---
+++ test.sql
@@ -0,0 +1,12 @@
+use `demo_csdl1`;
+-- create table Catalogs(
+-- id int primary key auto_increment ,
+-- `name` varchar(225),
+-- `description` text
+-- );
+-- select id `mã danh mục` from catalogs;
+select * from catalogs;
+-- INSERT INTO `demo_csdl1`.`catalogs` (`name`, `description`) VALUES ('alo', 'rat dep');
+insert into 
+
+
